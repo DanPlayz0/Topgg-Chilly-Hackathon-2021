6b6e8301a8347e8095451dcf9345c4ba937a5efb
caseyCantCode <77986712+caseyCantCode@users.noreply.github.com>
2021-12-09 19:48:52 -0700

    Delete src directory

--- src/commands/info/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "View the bot's ping!",
-  category: "info",
-  run: async (ctx) => {
-    ctx.interaction.followUp(`Pong! Bot ping: \`${ctx.client.ws.ping}\` ms!`);
-  },
-};
--- src/index.js
+++
@@ -1,6 +0,0 @@
-const Client = require("./structures/Client");
-require("dotenv").config();
-
-const client = new Client();
-
-client.start();
