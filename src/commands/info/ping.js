44ad676e4179f8c34e629b693937ecc12fc99253
caseyCantCode <77986712+caseyCantCode@users.noreply.github.com>
2021-12-09 17:46:53 -0700

    dan why do you hate me?

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,9 +1,8 @@
 module.exports = {
   name: "ping",
   description: "View the bot's ping!",
-  aliases: ["pong"],
   category: "info",
   run: async (ctx) => {
-    ctx.message.reply(`Pong! Bot ping: \`${ctx.client.ws.ping}\` ms!`);
+    ctx.interaction.followUp(`Pong! Bot ping: \`${ctx.client.ws.ping}\` ms!`);
   },
 };
