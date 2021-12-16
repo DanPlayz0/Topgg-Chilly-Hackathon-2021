7529f706b8389f64fad511cc2aea46676f2cd6ca
caseyCantCode <77986712+caseyCantCode@users.noreply.github.com>
2021-12-16 07:52:59 -0700

    Add files via upload

---
+++ src/commands/games/guess-the-gift.js
@@ -0,0 +1,49 @@
+const toys = [
+  "teddy bear",
+  "can of food",
+  "football",
+  "gaming controller",
+  "tea pot",
+  "new phone",
+  "new tablet",
+  "new puppy",
+];
+
+module.exports = {
+  name: "guess-the-gift",
+  description: "Try to guess what's inside the gift!",
+  category: "info",
+  run: async (ctx) => {
+    const selection = toys[Math.floor(Math.random() * toys.length)];
+
+    const msg = await ctx.interaction.followUp({
+      content: `Try to guess what's inside this gift!\n\nChoose from ${toys
+        .map((x) => `\`${x}\``)
+        .join(" | ")}\nYou have **30 seconds**`,
+    });
+    const collector = msg.channel.createMessageCollector({
+      filter: (m) => m.author.id === ctx.interaction.user.id,
+      time: 30000,
+    });
+
+    collector.on("collect", (message) => {
+      if (message.content.toLowerCase() === selection) {
+        ctx.interaction.followUp("You guessed the present good job!");
+        return collector.stop();
+      }
+
+      return collector.stop("fail");
+    });
+
+    collector.on("end", (_, reason) => {
+      if (reason === "fail")
+        return ctx.interaction.followUp(
+          `You didn't name the present correctly it was \`${selection}\`.`
+        );
+
+      return ctx.interaction.followUp(
+        `You ran out of time... The correct present was \`${selection}\`.`
+      );
+    });
+  },
+};
