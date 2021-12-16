1fa4d2aa2ca00184968bbcda76d27f5f338833a4
DanPlayz <damuffin36@gmail.com>
2021-12-16 00:22:49 -0800

    Setup for production

--- src/events/ready.js
+++ src/events/ready.js
@@ -12,22 +12,11 @@ module.exports = {
     );
 
     // Only certain guilds will have the faster updating commands
-    ["783178035322159156"].every((x) =>
-      client.guilds.cache
-        .get(x)
-        .commands.set(
-          client.commands
-            .toJSON()
-            .map((m) => ({
-              name: m.name,
-              description: m.description,
-              options: m.options,
-            }))
-        )
-    );
+    // ['783178035322159156'].every((x) => 
+    //   client.guilds.cache.get(x).commands.set(client.commands.toJSON().map((m) => ({ name: m.name, description: m.description, options: m.options, }))))
 
     // Uncomment the following line for when the bot is public.
-    // client.application.commands.set(client.commands.toJSON().map((m) => ({ name: m.name, description: m.description, options: m.options, })))
+    client.application.commands.set(client.commands.toJSON().map((m) => ({ name: m.name, description: m.description, options: m.options, })))
 
     client.user.setPresence({
       activities: [{ type: "WATCHING", name: "you celebrate" }],
